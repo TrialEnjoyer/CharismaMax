cfbd655cfa8b639e96e4b310f5b3f62c272b7d54
Adrian H <dev.adrianhankin@gmail.com>
2023-11-12 08:37:09 +1000

    N

--- apps/nextjs/src/pages/api/asdfg.ts
+++ apps/nextjs/src/pages/api/asdfg.ts
@@ -1,6 +1,30 @@
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import OpenAI from "openai";
 
 export const asdfg = async (input) => {
-  const supabase = useSupabaseClient().auth.admin;
+  //  const openai = new OpenAI();
+  //const supabase = useSupabaseClient().auth.admin;
   console.log(input);
+  //const completion = await openai.chat.completions.create({
+
+  //})
 };
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      // Call your function with the request body or other data
+      await asdfg(req.body);
+
+      // Send a response back
+      res.status(200).json({ message: "Function executed successfully" });
+    } catch (error) {
+      // Handle errors
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    // Handle non-POST requests
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}

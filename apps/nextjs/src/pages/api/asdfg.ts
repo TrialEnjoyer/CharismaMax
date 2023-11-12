f576fbb34255867e15999c9ed99f4917c75a729a
Adrian H <dev.adrianhankin@gmail.com>
2023-11-12 21:31:14 +1000

    Added a small console.log on the status 500 handler which probably should have already been there ages ago.

--- apps/nextjs/src/pages/api/asdfg.ts
+++ apps/nextjs/src/pages/api/asdfg.ts
@@ -86,6 +86,7 @@ export default async function handler(req, res) {
       // Send a response back
       res.status(200).json({ message: "Function executed successfully" });
     } catch (error) {
+      console.log(error);
       // Handle errors
       res.status(500).json({ error: "Internal Server Error" });
     }
